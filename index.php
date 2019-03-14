09aae48bb3e11b8fcd5a6ad371c1b2659de1e15d
Marcus Absher <mabsher@mail.greenriver.edu>
2019-03-14 16:03:51 -0400

    Submitting working version of final project

--- index.php
+++ index.php
@@ -12,8 +12,9 @@ error_reporting(E_ALL);
 
 //Require autoload
 require_once('vendor/autoload.php');
-require_once 'model/database.php';
 session_start();
+require_once('model/database.php');
+
 
 $valid = false;
 //create an instance of the Base class
@@ -24,3 +25,65 @@ $f3->set('DEBUG', 3);
 
 $data = new Database();
 $dbh = $data->connect();
+
+if(!$_SESSION['orderItems']) {
+    $_SESSION['orderItems'] = [];
+}
+
+
+
+//Define route
+$f3->route('GET|POST /',
+    function($f3) {
+
+        if(isset($_POST['submit'])) {
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            $email = $_POST['email'];
+            $address = $_POST['address'];
+            $city = $_POST['city'];
+            $state = $_POST['state'];
+            $zip = $_POST['zip'];
+            $phone = $_POST['phone'];
+            $password = $_POST['password'];
+
+            $newCustomer = new Customer($fname, $lname, $address, $city, $state, $zip, $phone, $email, $password);
+            //$data = new Database();
+            Database::insertCustomer($newCustomer);
+
+            $_SESSION['newCustomer'] = $newCustomer;
+            $f3->reroute('/drinkOrder');
+
+        }
+        //echo '<h1>My Dating Website</h1>';
+        $view = new View;
+        echo $view->render('views/signUp.html');
+    }
+);
+
+$f3->route('GET|POST /drinkOrder',
+    function($f3) {
+        $total = 0;
+        $customerInfo = $_SESSION['newCustomer'];
+        $menuItems = Database::getMenuItems();
+        $f3->set('menuItems', $menuItems);
+        $f3->set('total', $total);
+        $f3->set('orderItems',$_SESSION['orderItems'] );
+        //print_r($_POST);
+
+        if(isset($_POST['submit'])) {
+            $itemID = $_POST['submit'];
+            $orderItems = $_SESSION['orderItems'];
+            array_push($orderItems,$itemID);
+            $_SESSION['orderItems'] = $orderItems;
+            $f3->set('orderItems',$orderItems );
+        }
+
+        $template = new Template();
+
+        echo $template->render('views/drinkOrder.html');
+    });
+
+
+
+$f3->run();
--- model/database.php
+++ model/database.php
@@ -18,41 +18,31 @@ class Database
         }
     }
 
-    public static function insertMember($newMem)
+    public static function insertCustomer($newMem)
     {
         $newAdd = $newMem;
-        $premium = 0;
-        if (get_class($newAdd) == 'PremiumMember'){
-            $premium = 1;
-            $interests = array_merge($newAdd->getInDoorInterests(),$newAdd->getOutDoorInterests());
-            $interests = implode(",", $interests);
-        }else{
-            $interests = "";
-        }
-        //$interests = array_merge((array)$newAdd->indoors, (array)$newAdd->outdoors);
-
 
         global $dbh;
         //1. define the query
-        $sql = "INSERT INTO Members (fname, lname, age, gender, phone, email, state, seeking, bio, interests, premium)
-            VALUES (:fname, :lname, :age, :gender, :phone, :email, :state, :seeking, :bio, :interests, :premium)";
-       /* $sql = "INSERT INTO Members (fname, lname, age, gender, phone, email, state, seeking, bio, interests, premium)
-            VALUES (:fname, :lname, :age, :gender, :phone, :email, :state, :seeking, :bio, :interests, :premium)";*/
+        $sql = "INSERT INTO customers (emailAddress, password, firstName, lastName, address, city, state, zipCode, phone, account)
+            VALUES (:emailAddress, :password, :firstName, :lastName, :address, :city, :state, :zipCode, :phone, :account)";
+        /* $sql = "INSERT INTO Members (fname, lname, age, gender, phone, email, state, seeking, bio, interests, premium)
+             VALUES (:fname, :lname, :age, :gender, :phone, :email, :state, :seeking, :bio, :interests, :premium)";*/
         //2. prepare the statement
 
         $statement = $dbh->prepare($sql);
         //3. bind parameters
-        $statement->bindParam(':fname', $newAdd->getFName(), PDO::PARAM_STR);
-        $statement->bindParam(':lname', $newAdd->getLName(), PDO::PARAM_STR);
-        $statement->bindParam(':age', $newAdd->getAge(), PDO::PARAM_STR);
-        $statement->bindParam(':gender', $newAdd->getGender(), PDO::PARAM_STR);
-        $statement->bindParam(':phone', $newAdd->getPhone(), PDO::PARAM_STR);
-        $statement->bindParam(':email', $newAdd->getEmail(), PDO::PARAM_STR);
+        $statement->bindParam(':firstName', $newAdd->getFirstName(), PDO::PARAM_STR);
+        $statement->bindParam(':lastName', $newAdd->getLastName(), PDO::PARAM_STR);
+        $statement->bindParam(':address', $newAdd->getAddress(), PDO::PARAM_STR);
+        $statement->bindParam(':city', $newAdd->getCity(), PDO::PARAM_STR);
         $statement->bindParam(':state', $newAdd->getState(), PDO::PARAM_STR);
-        $statement->bindParam(':seeking', $newAdd->getSeeking(), PDO::PARAM_STR);
-        $statement->bindParam(':bio', $newAdd->getBio(), PDO::PARAM_STR);
-        $statement->bindParam(':interests', $interests, PDO::PARAM_STR);
-        $statement->bindParam(':premium', $premium, PDO::PARAM_STR);
+        $statement->bindParam(':zipCode', $newAdd->getZipCode(), PDO::PARAM_STR);
+        $statement->bindParam(':phone', $newAdd->getPhone(), PDO::PARAM_STR);
+        $statement->bindParam(':emailAddress', $newAdd->getEmail(), PDO::PARAM_STR);
+        $statement->bindParam(':password', $newAdd->getPassword(), PDO::PARAM_STR);
+        $statement->bindParam(':account', $newAdd->getAccount(), PDO::PARAM_INT);
+
 
         //4. execute the statement
         $success = $statement->execute();
@@ -63,11 +53,11 @@ class Database
     /**
      * @return array
      */
-    public static function getMembers()
+    public static function getCustomers()
     {
         global $dbh;
         //1. define the query
-        $sql = "SELECT * FROM Members ORDER BY lname";
+        $sql = "SELECT * FROM customers ORDER BY lastName";
         //2. prepare the statement
         $statement = $dbh->prepare($sql);
         //3. bind parameters
@@ -86,12 +76,51 @@ class Database
         return $result;
     }
 
-    public static function getMemberID($id)
+    public static function getCustomerID($id)
     {
 
         global $dbh;
         //1. define the query
-        $sql = "SELECT * FROM Members WHERE member_id=:id";
+        $sql = "SELECT * FROM customers WHERE customerID=:id";
+        //2. prepare the statement
+        $statement = $dbh->prepare($sql);
+        //3. bind parameters
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        //4. execute the statement
+        $statement->execute();
+        //5. return the result
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        //print_r($result);
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getMenuItems()
+    {
+
+        global $dbh;
+        //1. define the query
+        $sql = "SELECT * FROM menuItem ORDER BY categoryID";
+        //2. prepare the statement
+        $statement = $dbh->prepare($sql);
+        //3. bind parameters
+
+        //4. execute the statement
+        $statement->execute();
+        //5. return the result
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public static function getMenuItemByID($id)
+    {
+
+        global $dbh;
+        //1. define the query
+        $sql = "SELECT * FROM menuItem WHERE itemID=:id";
         //2. prepare the statement
         $statement = $dbh->prepare($sql);
         //3. bind parameters
