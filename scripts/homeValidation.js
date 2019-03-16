43732007022a4033a0d1efad0d232feb7d3d0d6d
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-03-16 14:49:51 -0400

    homepage login client-side js validation

--- scripts/homeValidation.js
+++ scripts/homeValidation.js
@@ -1,5 +1,5 @@
 //add spans with errors into html do display
-document.getElementById("pizzaform").onsubmit = validate;
+document.getElementById("loginForm").onsubmit = validate;
 
 function validate()
 {
@@ -10,9 +10,14 @@ function validate()
     var password = document.getElementById("password").value;
     if (email == "" || password == "")
     {
-        var loginError = document.getElementById("loginError");
-        loginError.style.visibility = "visible";
+        // var loginError = document.getElementById("loginError");
+        // loginError.style.visibility = "visible";
+        alert("Email or password is incorrect. Please try again.");
         isValid = false;
     }
 
+    if (!isValid)
+    {
+        return;
+    }
 }
